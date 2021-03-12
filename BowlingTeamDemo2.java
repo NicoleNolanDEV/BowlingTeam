3fe31402522bb1e806ee02f3ba688c4af9b61912
NicoleNolanDEV <76064733+NicoleNolanDEV@users.noreply.github.com>
2021-03-11 19:09:43 -0500

    Add files via upload
    Initial commit

---
+++ BowlingTeamDemo2.java
@@ -0,0 +1,44 @@
+//chapter 8 tutorial project 3/2
+
+import java.util.*;
+public class BowlingTeamDemo2
+{
+	public static void main(String[] args)
+	{
+		String name;
+		final int NUM_TEAMS = 4; 
+		BowlingTeam[] teams = new BowlingTeam[NUM_TEAMS];
+		int x;
+		int y;
+		final int NUM_TEAM_MEMBERS = 4;
+		Scanner input = new Scanner(System.in);
+
+		for(y = 0; y < NUM_TEAMS; ++y)
+		{
+
+			teams[y] = new BowlingTeam();
+			System.out.print("Please enter team name >> ");
+			name = input.nextLine();
+			teams[y].setTeamName(name);
+
+			for(x = 0; x < NUM_TEAM_MEMBERS; ++x)
+			{
+				System.out.print("Enter member name >> ");
+				name = input.nextLine();
+				teams[y].setMember(x, name);
+			}
+		}
+
+		
+		for(y = 0; y < NUM_TEAMS; ++y)
+		{
+
+			System.out.println("\nMembers of team " + teams[y].getTeamName());
+			for(x = 0; x < NUM_TEAM_MEMBERS; ++x)
+			{
+				System.out.print(teams[y].getMember(x) + " ");
+			}		
+			System.out.println();
+		}
+	}
+}
